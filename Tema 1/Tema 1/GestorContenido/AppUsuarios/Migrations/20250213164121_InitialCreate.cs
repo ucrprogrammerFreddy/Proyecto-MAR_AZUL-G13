5f6d7ad2053f669a583bd968780106962e193762
FREDDY.SANCHEZZAMORA <FREDDY.SANCHEZZAMORA@ucr.ac.acr>
2025-02-13 14:21:01 -0600

    Subir proyecto inicial

---
+++ Tema 1/Tema 1/GestorContenido/AppUsuarios/Migrations/20250213164121_InitialCreate.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppUsuarios.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Etiquetas",
+                columns: table => new
+                {
+                    IdEtiqueta = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Etiquetas", x => x.IdEtiqueta);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Secciones",
+                columns: table => new
+                {
+                    IdSeccion = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ImagenURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Estado = table.Column<string>(type: "nvarchar(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Secciones", x => x.IdSeccion);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Clave = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Rol = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Restablecer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.IdUsuario);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    IdCategoria = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(600)", maxLength: 600, nullable: false),
+                    UrlImagen = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    IdSeccion = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.IdCategoria);
+                    table.ForeignKey(
+                        name: "FK_Categorias_Secciones_IdSeccion",
+                        column: x => x.IdSeccion,
+                        principalTable: "Secciones",
+                        principalColumn: "IdSeccion",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    IdArticulo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    ImagenURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Estado = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    IdCategoria = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.IdArticulo);
+                    table.ForeignKey(
+                        name: "FK_Articulos_Categorias_IdCategoria",
+                        column: x => x.IdCategoria,
+                        principalTable: "Categorias",
+                        principalColumn: "IdCategoria",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArticuloAutor",
+                columns: table => new
+                {
+                    IdArticulo = table.Column<int>(type: "int", nullable: false),
+                    IdUsuario = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticuloAutor", x => new { x.IdArticulo, x.IdUsuario });
+                    table.ForeignKey(
+                        name: "FK_ArticuloAutor_Articulos_IdArticulo",
+                        column: x => x.IdArticulo,
+                        principalTable: "Articulos",
+                        principalColumn: "IdArticulo",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArticuloAutor_Usuarios_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "IdUsuario",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArticuloEtiqueta",
+                columns: table => new
+                {
+                    IdEtiqueta = table.Column<int>(type: "int", nullable: false),
+                    IdArticulo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticuloEtiqueta", x => new { x.IdArticulo, x.IdEtiqueta });
+                    table.ForeignKey(
+                        name: "FK_ArticuloEtiqueta_Articulos_IdArticulo",
+                        column: x => x.IdArticulo,
+                        principalTable: "Articulos",
+                        principalColumn: "IdArticulo");
+                    table.ForeignKey(
+                        name: "FK_ArticuloEtiqueta_Etiquetas_IdEtiqueta",
+                        column: x => x.IdEtiqueta,
+                        principalTable: "Etiquetas",
+                        principalColumn: "IdEtiqueta");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "IdUsuario", "Clave", "Email", "Estado", "Nombre", "Restablecer", "Rol" },
+                values: new object[] { 1, "abcdef", "Useradmin@gmail.com", "Activo", "User", "Realizado", "Admin" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticuloAutor_IdUsuario",
+                table: "ArticuloAutor",
+                column: "IdUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticuloEtiqueta_IdEtiqueta",
+                table: "ArticuloEtiqueta",
+                column: "IdEtiqueta");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articulos_IdCategoria",
+                table: "Articulos",
+                column: "IdCategoria");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categorias_IdSeccion",
+                table: "Categorias",
+                column: "IdSeccion");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArticuloAutor");
+
+            migrationBuilder.DropTable(
+                name: "ArticuloEtiqueta");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Articulos");
+
+            migrationBuilder.DropTable(
+                name: "Etiquetas");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "Secciones");
+        }
+    }
+}
---
+++ Tema 1/Tema 1/GestorContenido/AppUsuarios/Models/DbContextGestorContenidos.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppUsuarios.Models
+{
+    // Contexto de base de datos que gestiona la interacción con la base de datos mediante Entity Framework Core.
+    public class DbContextGestionContenido : DbContext
+    {
+        // Constructor que recibe opciones de configuración para la base de datos.
+        // Estas opciones son proporcionadas desde la configuración de la aplicación (en Program.cs).
+        // Esto es esencial porque permite flexibilidad al definir el proveedor de base de datos (SQL Server, SQLite, etc.).
+        //Es imprescindible, ya que sin esto, DbContext no sabe cómo conectarse a la base de datos.
+        public DbContextGestionContenido(DbContextOptions<DbContextGestionContenido> options) : base(options)
+        {
+        }// Cierre del constructor
+
+        // Representa la tabla de usuarios en la base de datos.
+        // Esencial para permitir consultas y manipulaciones de datos en la tabla "Usuarios".
+        // Es imprescindible, ya que sin esta propiedad, la aplicación no podría interactuar con la tabla.
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Articulos> Articulos { get; set; }
+        public DbSet<Categorias>Categorias { get; set; }
+        public DbSet<Etiquetas> Etiquetas { get; set; }
+        public DbSet<Secciones> Secciones { get; set; }
+        public DbSet<ArticuloAutor> ArticuloAutor { get; set; }
+
+
+        // Método que se ejecuta al crear el modelo de la base de datos.
+        // Se usa para configurar relaciones, restricciones y datos semilla.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+
+            // Definir la clave primaria compuesta para ArticuloEtiqueta
+            modelBuilder.Entity<ArticuloAutor>()
+                .HasKey(ae => new { ae.IdArticulo, ae.IdUsuario });
+
+            // Relación entre ArticulosAutor y Articulo
+            modelBuilder.Entity<ArticuloAutor>()
+                .HasOne(aa => aa.Articulo)
+                .WithMany(a => a.ArticuloAutor)
+                .HasForeignKey(ae => ae.IdArticulo);
+
+            modelBuilder.Entity<ArticuloAutor>()
+                .HasOne(au => au.Usuario)
+                .WithMany(a => a.ArticuloAutor)
+                .HasForeignKey(ae => ae.IdUsuario);
+
+            // Relacion Articulo Autor - Articulos
+
+            // Relación DetallePedido - Producto
+            modelBuilder.Entity<ArticuloAutor>()
+                .HasOne(dp => dp.Articulo)
+                .WithMany(aa => aa.ArticuloAutor)
+                .HasForeignKey(dp => dp.IdArticulo);
+
+          
+               
+            // Categoria -articulo
+            modelBuilder.Entity<Articulos>()
+                .HasOne(a => a.Categoria)
+                .WithMany(c => c.Articulo)
+                .HasForeignKey(a => a.IdCategoria);
+
+            // relacion categoria seccion
+            modelBuilder.Entity<Categorias>()
+              .HasOne(c => c.Seccion)
+              .WithMany(s => s.Categoria)
+              .HasForeignKey(a => a.IdSeccion);
+
+            // Definir la clave primaria compuesta para ArticuloEtiqueta
+            modelBuilder.Entity<ArticuloEtiqueta>()
+                .HasKey(ae => new { ae.IdArticulo, ae.IdEtiqueta });
+
+            // Relación entre Articulos y Etiquetas (ArticuloEtiqueta)
+
+            modelBuilder.Entity<ArticuloEtiqueta>()
+            .HasOne(ae => ae.Articulo)
+            .WithMany(a => a.ArticuloEtiqueta)
+            .HasForeignKey(ae=> ae.IdArticulo)
+            .OnDelete(DeleteBehavior.NoAction); // Prevenir eliminación en cascada
+
+            modelBuilder.Entity<ArticuloEtiqueta>()
+                .HasOne(ae => ae.Etiqueta)
+                .WithMany(e => e.ArticuloEtiqueta)
+                .HasForeignKey(ae =>ae.IdEtiqueta)
+                .OnDelete(DeleteBehavior.NoAction); // Prevenir eliminación en cascada
+
+
+            
+
+            // Agregamos datos iniciales a la tabla Usuarios para que existan al momento de crear la base de datos.
+            // Esto es útil para pruebas o configuraciones iniciales.
+            // Si la aplicación no necesita datos iniciales, esta parte del código puede omitirse.
+            modelBuilder.Entity<Usuario>().HasData
+            (new Usuario()
+            {
+                IdUsuario = 1,
+                Nombre = "User",
+                Email = "Useradmin@gmail.com",
+                Clave = "abcdef", // Contraseña no segura (solo como ejemplo)
+                Estado = "Activo",
+                Rol = "Admin",
+                Restablecer = "Realizado"
+            }
+            );
+        }// Cierre del método OnModelCreating
+    } // Cierre de la clase
+} // Cierre del namespace
